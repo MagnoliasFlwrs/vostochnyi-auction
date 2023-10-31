ad097946bb69a8f91067eb8868e3d1685f048f49
Savenok Marta <magnolia.sav@gmail.com>
2023-10-31 23:32:49 +0300

    end

---
+++ src/assets/script/script.js
@@ -0,0 +1,8 @@
+// burger
+
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+burgerBtn.addEventListener('click' , ()=> {
+    burgerBtn.classList.toggle('active');
+    burgerMenu.classList.toggle('open');
+})
