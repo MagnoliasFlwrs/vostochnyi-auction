3403a10132a101fce1be9be32ce71b6feb605162
Savenok Marta <magnolia.sav@gmail.com>
2023-11-03 23:25:59 +0300

    end

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -6,3 +6,47 @@ burgerBtn.addEventListener('click' , ()=> {
     burgerBtn.classList.toggle('active');
     burgerMenu.classList.toggle('open');
 })
+
+//sort
+
+const sortBtns = document.querySelectorAll('.sort-btn');
+
+sortBtns.forEach(el=> {
+    el.addEventListener('click' , ()=> {
+        el.classList.toggle('active');
+    })
+})
+
+// tabs
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const tabsBtns = document.querySelectorAll('.tabs-panel li');
+    const tabs = document.querySelectorAll('.tab-container .tab-content');
+
+
+    if(tabsBtns){
+      tabsBtns.forEach(function(el){
+        el.addEventListener('click', function(ev){
+          makeActive(ev.target);
+        });
+      });
+    }
+  function makeActive(target){
+    let num = target.dataset.num;
+    let selector = '.tab-container .tab-content[data-num="'+num+'"]';
+    let targetTab = document.querySelector(selector);
+
+    tabsBtns.forEach(function(el){
+      el.classList.remove('active');
+    });
+    tabs.forEach(function(el){
+      el.classList.remove('active');
+    });
+    target.classList.add('active');
+    targetTab.classList.add('active');
+
+  }
+
+
+  });
+
